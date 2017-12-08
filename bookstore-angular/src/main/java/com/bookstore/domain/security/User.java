f9d44d8b2960356128b66b54ab8a2dfbf55466e8
mahugnon <homahugnon@gmail.com>
2017-12-09 00:47:51 +0100

    first commit

---
+++ bookstore-angular/src/main/java/com/bookstore/domain/security/User.java
@@ -0,0 +1,126 @@
+package com.bookstore.domain.security;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class User implements UserDetails,Serializable{
+@Id
+@GeneratedValue(strategy=GenerationType.AUTO)
+@Column(name="id", nullable=false, updatable=false)
+	private Long id; 
+private String username;
+private String password;
+private String firstName;
+private String lastName;
+
+private String email;
+private String phone;
+private boolean enabled=true;
+@OneToMany(mappedBy="user", cascade=CascadeType.ALL,fetch=FetchType.EAGER)
+@JsonIgnore
+private Set<UserRole> userRoles=new HashSet<>();
+public User() {
+	super();
+}
+public User(String username, String password, String firstName, String lastName, String email, String phone,
+		boolean enabled) {
+	super();
+	this.username = username;
+	this.password = password;
+	this.firstName = firstName;
+	this.lastName = lastName;
+	this.email = email;
+	this.phone = phone;
+	this.enabled = enabled;
+}
+public Long getId() {
+	return id;
+}
+public void setId(Long id) {
+	this.id = id;
+}
+public String getUsername() {
+	return username;
+}
+public void setUsername(String username) {
+	this.username = username;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+public String getFirstName() {
+	return firstName;
+}
+public void setFirstName(String firstName) {
+	this.firstName = firstName;
+}
+public String getLastName() {
+	return lastName;
+}
+public void setLastName(String lastName) {
+	this.lastName = lastName;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public String getPhone() {
+	return phone;
+}
+public void setPhone(String phone) {
+	this.phone = phone;
+}
+
+public void setEnabled(boolean enabled) {
+	this.enabled = enabled;
+}
+public Set<UserRole> getUserRoles() {
+	return userRoles;
+}
+public void setUserRoles(Set<UserRole> userRoles) {
+	this.userRoles = userRoles;
+}
+@Override
+public Collection<? extends GrantedAuthority> getAuthorities() {
+Set<GrantedAuthority> authorities=new HashSet<>();
+userRoles.forEach(ur->authorities.add(new Authority(ur.getRole().getName())));
+	return authorities;
+}
+@Override
+public boolean isAccountNonExpired() {
+	return true;
+}
+@Override
+public boolean isAccountNonLocked() {
+	return true;
+}
+@Override
+public boolean isCredentialsNonExpired() {
+	return true;
+}
+@Override
+public boolean isEnabled() {
+	return enabled;
+}
+
+}
